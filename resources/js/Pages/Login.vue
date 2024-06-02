d8ad327b05ba5d6c5cacc3e9bda9856c28abdf26
Jasper Fernandez <jafernandez@nemsu.edu.ph>
2024-06-02 10:49:56 +0800

    chore: Update page title and route for login page

---
+++ resources/js/Pages/Login.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="h-screen flex justify-center items-center bg-gray-200">
+        <div class="bg-gray-500 w-[600px]">
+            <h1 class="font-bold text-center">Login</h1>
+        </div>
+    </div>
+</template>
